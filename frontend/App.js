8fdc0fdc138ac7ed474df58e19f5a1cc3fbe737d
Sujit Singh <114153271+sujitsiingh@users.noreply.github.com>
2024-06-23 12:54:11 +0530

    App.js updt

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './Screens/HomeScreen'
+import LoginScreen from './Screens/LoginScreen';
+import RegisterScreen from './Screens/RegisterScreen';
+import HomeScreen2 from './Screens/user/HomeScreen2';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <StatusBar style="auto" /> */}
+      {/* <HomeScreen /> */}
+      {/* <LoginScreen/> */}
+      {/* <RegisterScreen/> */}
+      <HomeScreen2/>
     </View>
   );
 }
